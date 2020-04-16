38a661a198d7cd690aed8061b9f85d8e646d0ca8
Anuj Kumar <iitr.anujkumar@gmail.com>
2020-04-17 03:20:53 +0530

    day11 solution

--- day11/day11.cpp
+++ day11/day11.cpp
@@ -1,18 +1,32 @@
-class Day11 {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
     int ans = 0;
-    int helper(TreeNode root) {
-        if(root == null) return 0;
-        
-        int hL = helper(root.left);
-        int hR = helper(root.right);
-        
-        ans = Math.max(ans,hL+hR);
-        return 1 + Math.max(hL,hR);
+    int height(TreeNode* root) {
+        if(root == NULL) return 0;
+        return 1 + max(height(root->left),height(root->right));
     }
-    public int diameterOfBinaryTree(TreeNode root) {
-        if(root == null)
-            return 0;
-        int height = helper(root);
-        return ans;
+    void myFun(TreeNode* root) {
+        if(root == NULL) return;
+        
+        int hL = height(root->left);
+        int hR = height(root->right);
+        ans = max(ans,hL+hR+1);
+        myFun(root->left);
+        myFun(root->right);
     }
-}
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        ans = 0;
+        if(root == NULL) return 0;
+        myFun(root);
+        return ans-1;
+    }
+};
